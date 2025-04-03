1465c0e01f25ec6e09e309399a4cc57207465b95
mjwu <mjay.atm@g.ncu.edu.tw>
2025-04-03 09:30:39 +0800

    Add: extract the snippet in PXPay (by CHPost)

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from transaction.CHPost import MobilePayment
 #%%
 if __name__ == '__main__':
     mp = MobilePayment('labels.json')
-    for key, value in mp.label_id.items():
-        print(key, value)
-    print(mp.PXPay)
+    target = mp.PXPay
+
+#%%
--- transaction/CHPost.py
+++ transaction/CHPost.py
@@ -1,7 +1,13 @@
 #%%
 from lib.gmail import MyGmailAPI
+from lib import extract_numbers_from_string as getnum
 import json
-
+#%%
+def to_date(date_list):
+    yyyy = date_list[0]
+    mm = date_list[1]
+    dd = date_list[2]
+    return f"{yyyy}-{mm}-{dd}"
 #%%
 class MobilePayment(object):
     def __init__(self, label_source):
@@ -16,8 +22,22 @@ class MobilePayment(object):
         self.PXPay = self._PXPay()
     def _PXPay(self):
         msg_id = self.service.list_msg(labelIds=self.label_id['PXPay'])
+        records = []
         for mid in msg_id:
             mi = mid['id']
             msg = self.service.get_msg(mi)['snippet']
-            print(msg)
-        return msg_id
+            if "交易金額" in msg:
+                records.append(self._ExtractSnippet(msg, 'CHPost'))
+        return records
+    def _ExtractSnippet(self, snippet:str, source:str):
+        record = {}
+        if (source == "CHPost"):
+            snippet = snippet.split('*')
+            record['ID'] = int(snippet[5].split(')')[0])
+            record['DT'] = to_date(getnum(snippet[-4]))
+            record['NO'] = getnum(snippet[-3])[0]
+            record['AM'] = int(getnum(snippet[-1].split()[0])[0])
+            return record
+        else:
+            raise ValueError(f"Unrecognized source: {source}")
+            
