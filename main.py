723094c357815afa46b4e51418da9f9eaf4dd116
mjwu <mjay.atm@g.ncu.edu.tw>
2025-04-03 23:31:37 +0800

    Add: Deposits in CHPost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #%%
-from transaction.CHPost import MobilePayment
+from transaction.CHPost import MobilePayment, Deposit
 
 #%%
 
@@ -9,5 +9,42 @@ if __name__ == '__main__':
     target1 = mp.PXPay
     target2 = mp.EZPay
     target3 = mp.iPass
-
 #%%
+    Month = '2025-03'
+    ##### PXPay amounts
+    amounts1 = 0
+    for t in target1:
+        if Month in t['DT']:
+            amounts1 += t['AM']
+
+    ##### EZPay amounts
+    amounts2 = 0
+    for t in target2:
+        if Month in t['DT']:
+            amounts2 += t['AM']
+
+    ##### iPass amounts
+    amounts3 = 0
+    for t in target3:
+        if Month in t['DT']:
+            amounts3 += t['AM']
+
+    print(f"MobilePay in {Month}:")
+    print(f"    PXPay: {amounts1:8} (NT$)")
+    print(f"    EZPay: {amounts2:8} (NT$)")
+    print(f"    iPass: {amounts3:8} (NT$)")
+#%%
+    dp = Deposit('labels.json').records
+    #%%
+    Month = "2025-03"
+    amount = {}
+    for r in dp:
+        if Month in r['DT']:
+            print(r)
+            if r['TP'] not in amount:
+                amount[r['TP']] = 0
+            amount[r['TP']] += r['AM']
+    print()
+    print(f"Deposits in {Month}:")
+    for k, v in amount.items():
+        print(f"    {k}: {v:8} (NT$)")
--- transaction/CHPost.py
+++ transaction/CHPost.py
@@ -3,12 +3,43 @@ from lib.gmail import MyGmailAPI
 from lib import extract_numbers_from_string as getnum
 import json
 #%%
-def to_date(date_list):
-    yyyy = date_list[0]
-    mm = date_list[1]
-    dd = date_list[2]
-    return f"{yyyy}-{mm}-{dd}"
+
 #%%
+class Deposit(object):
+    def __init__(self, label_source):
+        self.service = MyGmailAPI()
+        with open(label_source, 'r') as f:
+            labels = json.load(f)
+        for label in labels:
+            if 'Deposits' in label['name']:
+                self.label_id = label['id']
+                break
+        else:
+            raise ValueError("No label with 'Deposits' found in the provided labels.")
+        msg_id = self.service.list_msg(labelIds=self.label_id)
+        self.records = []
+        for mid in msg_id:
+            mi = mid['id']
+            msg = self.service.get_msg(mi)['snippet']
+            record = self._ExtractMessege(msg)
+            self.records.append(record)
+    def _to_datetime(self, date_msg:str, time_msg:str):
+        date_list = getnum(date_msg)
+        time_list = getnum(time_msg)
+        yyyy = int(date_list[0]) + 1911
+        mm, dd = date_list[1], date_list[2]
+        HH, MN = time_list[0], time_list[1]
+        return f'{yyyy:04}-{mm}-{dd}T{HH}:{MN}'
+    def _ExtractMessege(self, msg:str):
+        record = {}
+        snippet = msg.split()
+        record['TO'] = int(snippet[3][-5:])
+        record['TP'] = snippet[7][-4:]
+        record['FM'] = int(snippet[8][-5:])
+        record['DT'] = self._to_datetime(snippet[5], snippet[6])
+        record['AM'] = int(getnum(snippet[4])[0])
+        return record
+
 class MobilePayment(object):
     def __init__(self, label_source):
         self.service = MyGmailAPI()
@@ -49,12 +80,17 @@ class MobilePayment(object):
             if "交易金額" in msg:
                 records.append(self._ExtractSnippet(msg, 'CHPost'))
         return records
+    def to_date(self, date_list):
+        yyyy = date_list[0]
+        mm = date_list[1]
+        dd = date_list[2]
+        return f"{yyyy}-{mm}-{dd}"
     def _ExtractSnippet(self, snippet:str, source:str):
         record = {}
         if (source == "CHPost"):
             snippet = snippet.split('*')
             record['ID'] = int(snippet[5].split(')')[0])
-            record['DT'] = to_date(getnum(snippet[-4]))
+            record['DT'] = self.to_date(getnum(snippet[-4]))
             record['NO'] = getnum(snippet[-3])[0]
             record['AM'] = int(getnum(snippet[-1].split()[0])[0])
             return record
